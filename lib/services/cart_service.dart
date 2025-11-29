16bb7d0fb532087d7ece7a051af8ee1a66bdf833
Oussamabenazine <benazine.oussama@gmail.com>
2025-11-29 15:21:58 +0100

    Fixing some errors

--- lib/services/cart_service.dart
+++ lib/services/cart_service.dart
@@ -1,10 +1,8 @@
 // lib/services/cart_service.dart 
 import 'package:flutter/foundation.dart';
 // Import your service here:
-import 'firebase_product_service.dart'; 
 
 class CartService with ChangeNotifier { 
   // ... existing members
-  final FirebaseProductService _productService = FirebaseProductService(); 
   // ... existing methods (e.g., clearCart()) [cite: 202, 204]
 }
